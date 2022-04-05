051be2dd2e7f39860d0058aaabb0ee1dcb22b24d
krzychu <krzychu.biska2@gmail,com>
2022-04-05 09:16:11 +0200

    f

--- src/main.java
+++ src/main.java
@@ -1,12 +1,21 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-        ArrayList<zlecenie> zlecenia = Kolejki.kolejka_bez_priorytetów(8,100,50);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wiaj w symulatorze algorytmów dostępu do dysku!\nPodaj dlugość ciągu zleceń jakie algorytmy mają otrzymać");
+        int dlug = Integer.parseInt(scan.nextLine());
+        System.out.println("Podaj długość dysku (Adres ostatniej ścieżki)");
+        int cyl = Integer.parseInt(scan.nextLine());
+        System.out.println("Podaj maksymalny czas zgłoszenia się procesu");
+        int czas = Integer.parseInt(scan.nextLine());
+
+        ArrayList<zlecenie> zlecenia = Kolejki.kolejka_bez_priorytetów(dlug,cyl,czas);
         System.out.println(zlecenia);
         System.out.println(Algorytmy.FCFS(zlecenia));
-        zlecenia.sort(Comparator.comparingInt(zlecenie::getCzas_zgloszenia));
-        System.out.println(zlecenia);
+        System.out.println(Algorytmy.SSTF(zlecenia));
+
     }
 }
